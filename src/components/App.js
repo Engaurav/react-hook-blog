b59f8e84aee5f7ea58bdee6d5cb6ee92b4c7c081
Gaurav Singh <gaurav.singh323@gmail.com>
2022-05-26 12:49:51 +0530

    Add Structure to App

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import { Switch, Route } from "react-router-dom";
+import Navbar from './Navbar';
+import CreatePost from './CreatePost';
+import Home from "./Home";
+import PostDetail from "./PostDetail";
+function App() {
+  return (
+    <div className="container">
+      <Navbar/>
+      <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/post/:postId' component={PostDetail} />
+          <Route exact path='/createpost' component={CreatePost} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1VVWCgYohczEL8CQH_g6D0mC6n4oex3Q",
+  authDomain: "react-hooks-blog-f273c.firebaseapp.com",
+  projectId: "react-hooks-blog-f273c",
+  storageBucket: "react-hooks-blog-f273c.appspot.com",
+  messagingSenderId: "641612990415",
+  appId: "1:641612990415:web:2ef634634a60dd6af5f4ba"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
